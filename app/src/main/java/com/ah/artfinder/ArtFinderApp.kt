eb751234035e3d2208c5b1ff2ad247090eb8674b
Eman Wahba <emansafwat0@gmail.com>
2023-08-01 00:24:57 +0200

    [ADD] artFinder application with Hilt annotation [ADD] Hilt entryPoint annotation to MainActivity [ADD] application class path and internet permission to manifest file

---
+++ app/src/main/java/com/ah/artfinder/ArtFinderApp.kt
@@ -0,0 +1,7 @@
+package com.ah.artfinder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ArtFinderApp : Application()
--- app/src/main/java/com/ah/artfinder/MainActivity.kt
+++ app/src/main/java/com/ah/artfinder/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ah.artfinder.ui.theme.ArtFinderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
